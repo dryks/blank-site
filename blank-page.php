fc40eede71042885e5f1bcdfd2f6733cd7867bed
Olympus\Jacob <jamh.unm@gmail.com>
2016-12-11 17:23:05 -0700

    Created blank-page for template

---
+++ blank-page.php
@@ -0,0 +1,17 @@
+<?php include '_assets/includes/header.inc.php' ?>
+<!-- Opening Body tag is enclosed inside of header.inc.php 
+<body>
+<div class="container">
+  <div class="row">
+-->
+    <h1>Blank Page Template</h1>
+<!--  
+	</div>
+</div>
+-->
+<?php include '_assets/includes/footer.inc.php' ?>
+<!-- Page Specific JS --> 
+
+<!-- End Page Specific JS -->
+</body>
+</html>
