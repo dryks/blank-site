f9423f47de7258e26df21d8af84c9711add806b3
Olympus\Jacob <jamh.unm@gmail.com>
2016-12-11 18:26:57 -0700

    updated contact form layout

--- contact.php
+++ contact.php
@@ -5,7 +5,7 @@ $missing = array();
 if (isset($_POST['send'])) {
   // email processing script
   $visitor_email = $_POST['email'];
-  $to = 'info@company.com';
+  $to = 'infotest@mailinator.com';
   $subject = 'Feedback from Company';
   // list expected fields
   $expected = array('name', 'phone', 'email', 'subject', 'comments');
@@ -34,21 +34,24 @@ if (isset($_POST['send'])) {
 }
 ?>
 <?php include '_assets/includes/header.inc.php' ?>
-<div class="col-md-8 col-md-offset-2">
-  <h1>Contact</h1>
-  <h4>Please be sure to include a <strong>valid email address</strong></h4>
-  <hr />
-  <form id="feedback" method="post" action="">
-    <?php if (($_POST && $suspect) || ($_POST && isset($errors['mailfail']))) { ?>
-    <p class="warning">Sorry, your mail could not be sent. Please try later.</p>
-    <?php } elseif ($missing || $errors) { ?>
-    <h3 class="text-error text-center">Please fix the item(s) indicated</h3>
-    <?php } ?>
-      <div class="form-group">
-        <label>
+    <div class="col-md-8 col-md-offset-2">
+      <h1>Contact</h1>
+      <h4>Please be sure to include a <strong>valid email address</strong></h4>
+      <hr />
+      <form id="feedback" method="post" action="">
+        <?php if (($_POST && $suspect) || ($_POST && isset($errors['mailfail']))) { ?>
+        <p class="warning">Sorry, your mail could not be sent. Please try later.</p>
+        <?php } elseif ($missing || $errors) { ?>
+        <div class="alert alert-danger text-center">
+          <h3 class="text-error text-center">Please fix the item(s) indicated</h3>
+        </div>
+        <?php } ?>
+        <div class="form-group">
+          <label>
           <?php 
             if ($missing && in_array('name', $missing)) { ?>
-            <span class="text-error">Please enter your name</span>
+          <div class="alert alert-danger" role="alert">Name Required</div>
+          <!--<span class="text-error">Please enter your name</span>-->
           <?php 
             } else {
           ?>
@@ -56,78 +59,73 @@ if (isset($_POST['send'])) {
           <?php 
             } 
           ?>
-        </label>
-        <input name="name" id="name" type="text" class="form-control" placeholder="Name *Required*">
-      </div>
-      <div class="form-group">
-        <label>Phone Number</label>
-        <input name="phone" id="phone" type="tel" class="form-control" placeholder="Phone *Optional*">
-      </div>
-      <div class="form-group">
-        <label>
-          <?php 
+          </label>
+          <input name="name" id="name" type="text" class="form-control" placeholder="Name *Required*">
+        </div>
+        <div class="form-group">
+          <label>Phone Number</label>
+          <input name="phone" id="phone" type="tel" class="form-control" placeholder="Phone *Optional*">
+        </div>
+        <div class="form-group">
+          <label>
+            <?php 
         if ($missing && in_array('email', $missing)) { 
         ?>
-          <span class="text-error">Please enter your email address</span>
-          <?php 
+            <span class="text-error">Please enter your email address</span>
+            <?php 
         } elseif (isset($errors['email'])) { 
         ?>
-            <span class="text-warning">Invalid email address</span>
-            <?php 
+              <span class="text-warning">Invalid email address</span>
+              <?php 
         } else { 
         ?>
-            Email Address:
-            <?php 
+              Email Address:
+              <?php 
         } 
         ?>
-        </label>
-        <input name="email" id="email" type="text" class="form-control" placeholder="Email Address *Required*">
-      </div>
-      <div class="form-group">
-        <label>Subject</label>
-        <select id="subject" name="subject" class="form-control">
-        
-          <option value="na">Choose one:</option>
-          
-          <option value="option1"
+          </label>
+          <input name="email" id="email" type="text" class="form-control" placeholder="Email Address *Required*">
+        </div>
+        <div class="form-group">
+          <label>Subject</label>
+          <select id="subject" name="subject" class="form-control">
+            <option value="na">Choose one:</option>
+            <option value="option1"
       <?php
       if ($_POST && $_POST['subject'] == 'option1') {
         echo 'selected';
       } ?>>Option 1</option>
-      
-          <option value="option2"
+            <option value="option2"
       <?php
       if ($_POST && $_POST['subject'] == 'option2') {
         echo 'selected';
       } ?>>Option 2</option>
-      
-          <option value="option3"
+            <option value="option3"
       <?php
       if ($_POST && $_POST['subject'] == 'option3') {
         echo 'selected';
       } ?>>Option 3</option>
-        </select>
-        
-      </div>
-      <div class="form-group">
-        <label>
-          <?php 
+          </select>
+        </div>
+        <div class="form-group">
+          <label>
+            <?php 
       if ($missing && in_array('comments', $missing)) { ?>
-          <span class="text-error">Please enter your comments</span>
-          <?php 
+            <span class="text-error">Please enter your comments</span>
+            <?php 
       } else { 
       ?>
-          Message:
-          <?php
+            Message:
+            <?php
       }
       ?>
-        </label>
-        <textarea name="comments" id="comments" class="form-control" rows="10" placeholder="Please enter your comments here..."></textarea>
-      </div>
-      <button type="submit" name="send" id="send" class="btn btn-inverse pull-right">Send</button>
-  </form>
-</div>
-<?php include '_assets/includes/footer.inc.php' ?>
+          </label>
+          <textarea name="comments" id="comments" class="form-control" rows="10" placeholder="Please enter your comments here..."></textarea>
+        </div>
+        <button type="submit" name="send" id="send" class="btn btn-inverse pull-right">Send</button>
+      </form>
+    </div>
+    <?php include '_assets/includes/footer.inc.php' ?>
 <!-- Page Specific JS --> 
 
 <!-- End Page Specific JS -->
